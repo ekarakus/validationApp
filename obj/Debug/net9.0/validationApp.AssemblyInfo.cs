c55bea015e05a744c87e580e2b3ce4d5807a5a04
Ergün Karakuş <EKarakus@btofis.com>
2025-05-28 11:52:24 +0300

    enctype eklendi

--- obj/Debug/net9.0/validationApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/validationApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("validationApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d47f3a936dd4d7459b1f92aca14df1e910f4610")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65a04a841278292457eaed467cafeb347e55b441")]
 [assembly: System.Reflection.AssemblyProductAttribute("validationApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("validationApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
